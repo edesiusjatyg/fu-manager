55f1771ad3d04e13dd14ff8c43b4d6f474192f6d
edesiusjatyg <edesiusjatyg@gmail.com>
2025-11-01 02:52:49 +0700

    be models and db configured

--- be/main.go
+++ be/main.go
@@ -1,9 +1,32 @@
 package main
 
 import (
+	"context"
 	"fmt"
+
+	"gorm.io/gorm"
+	"ventra.com/backend/dbconfig"
+	"ventra.com/backend/models"
 )
 
-func main(){
-	fmt.Println("hola")
+func main() {
+    db, err := dbconfig.ConnectDB()
+	if err != nil {
+		panic("Connection failed!")
+	}
+
+	db.AutoMigrate(
+		&models.LeadsTags{},
+		&models.LeadsData{},
+		&models.StatsData{},
+	)
+
+	ctx := context.Background()
+
+	tag, err := gorm.G[models.LeadsTags](db).Where("id = ?", 1).First(ctx)
+	if err != nil {
+		panic("Query failed!")
+	}
+
+	fmt.Println(tag.TagsTitle)
 }
