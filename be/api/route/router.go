602164f92a8c7293b79b7bc19d4929e99963ba82
edesiusjatyg <edesiusjatyg@gmail.com>
2025-11-02 00:49:05 +0700

    be - tags handler proto complete

--- be/api/route/router.go
+++ be/api/route/router.go
@@ -10,6 +10,7 @@ func SetupRouter(db *gorm.DB) *gin.Engine {
 	router := gin.Default()
 	statsHandler := handlers.InitStatsHandler(db)
 	leadsHandler := handlers.InitLeadsHandler(db)
+	tagsHandler := handlers.InitTagsHandler(db)
 
 	api := router.Group("/api")
 	{
@@ -21,6 +22,11 @@ func SetupRouter(db *gorm.DB) *gin.Engine {
 		api.POST("/leads", leadsHandler.CreateLead)
 		api.PUT("/leads/:id", leadsHandler.UpdateLead)
 		api.DELETE("/leads/:id", leadsHandler.DeleteLead)
+
+		api.GET("/tags", tagsHandler.GetTags)
+		api.GET("/tags/:id", tagsHandler.GetTag)
+		api.POST("/tags", tagsHandler.CreateTag)
+		api.DELETE("/tags/:id", tagsHandler.DeleteTag)
 	}
 
 	return router
