189367c85ecb0d2196200f302c5771f746494343
edesiusjatyg <edesiusjatyg@gmail.com>
2025-11-02 02:38:28 +0700

    be - added dot env

--- be/dbconfig/dbconfig.go
+++ be/dbconfig/dbconfig.go
@@ -1,15 +1,32 @@
 package dbconfig
 
 import (
+	"fmt"
+	"os"
+	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func ConnectDB() (*gorm.DB, error) {
-	dsn := "fast9:8099@tcp(34.101.195.31:3306)/ventra_lead_tracker?charset=utf8mb4&parseTime=True&loc=Local"
+	err := godotenv.Load()
+	if err != nil {
+		return nil, err
+	}
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", 
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_NAME"),
+	)
+	
   	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		return nil, err
 	}
+	fmt.Println("Database connected!")
 	return db, nil
+		
 }
